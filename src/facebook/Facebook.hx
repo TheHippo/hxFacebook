db5501fbbfea0a1655a12301042abc99414a321c
Philipp Klose <me@thehippo.de>
2010-10-06 19:28:27 +0200

    api calls work

--- src/facebook/Facebook.hx
+++ src/facebook/Facebook.hx
@@ -24,19 +24,57 @@ class Facebook {
 		inst = untyped __call__('new Facebook',Lib.associativeArrayOfHash(config));
 	}
 	
-	public function getSession() {
-		return untyped inst.getSession();
+	public function getSession():Session {
+		var d:Dynamic = inst.getSession();
+		return if (d==null)
+			null;
+		else
+			this.buildObject({},d);
 	}
 	
-	public function api<T>(query:String):T {
-		var d = inst.api(query);
-		Lib.dump(d);
-		var res = Lib.hashOfAssociativeArray(d);
-		var ret:Dynamic = {};
-		for (k in res.keys())
-			Reflect.setField(ret,k,res.get(k));
-		return ret;	
+	public function setSession(session:Session,?cookie:Bool=true) {
+		var h = new Hash<Dynamic>();
+		for (f in Reflect.fields(session))
+			h.set(f,Reflect.field(session,f));
+		inst.setSession(Lib.associativeArrayOfHash(h),cookie);
 	}
+	
+	inline function isIndexed(d:Dynamic):Bool
+		return untyped __physeq__(__call__('array_values',d),d)
+	
+	inline function isArray(d:Dynamic):Bool
+		return untyped __call__('is_array',d)
+		
+	function getValue(d:Dynamic)
+		return if (this.isArray(d))
+			this.buildObject({},d)
+		else
+			d
+	
+	function buildObject(dest:Dynamic,source:Dynamic):Dynamic {
+		untyped {
+			__php__('foreach ($source as $key=>$value) {');
+				if (!this.isArray(value)) {
+					Reflect.setField(dest,key,value);
+				}
+				else {
+					if (this.isIndexed(value)) {
+						var a = new Array<Dynamic>();
+						__php__('foreach ($value as $v){');
+							a.push(this.getValue(v));
+						__php__('}');
+						Reflect.setField(dest,key,a);
+					}
+					else
+						Reflect.setField(dest,key,this.buildObject({},value));
+				}			
+			__php__('}');		
+		}
+		return dest;
+	}
+	
+	public function api<T>(query:String):T
+		return this.buildObject({},inst.api(query))
 	
 	public function getUser():Int
 		return inst.getUser()
---
+++ src/facebook/Session.hx
@@ -0,0 +1,12 @@
+package facebook;
+
+typedef Session = {
+	session_key:String,
+	uid:String,
+	expires:Int,
+	secret:String,
+	base_domain:String,
+	access_token:String,
+	sig:String
+
+}
